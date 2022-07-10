70d8b436994c4063b6eb5e8cc4d3b7f17b0def6e
unknown <milosz20031207@gmail.com>
2022-07-10 19:05:44 +0200

    added layout for new project

--- src/components/Home/projects/MapProject.tsx
+++ src/components/Home/projects/MapProject.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import NewProject from "./NewProject";
 import Project from "./Project";
 import { styles } from "./ProjectStyles";
 
@@ -6,6 +7,7 @@ const MapProject: FC = () => {
 
     return (
         <div className={ styles.map_project }>
+            <NewProject/>
             {
                 Array(10).fill( { title: 'lorem', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" } )
                 .map( ( { title, content }, ind ) => (
---
+++ src/components/Home/projects/NewProject.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { styles } from "./ProjectStyles";
+
+const NewProject: FC = () => {
+
+    return (
+        <div className={ styles.new_project }>
+            +
+        </div>
+    )
+}
+
+export default NewProject
