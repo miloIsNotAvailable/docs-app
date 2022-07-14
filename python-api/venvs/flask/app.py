c4961049eb3c9854b950d31d30540299ff0f6587
unknown <milosz20031207@gmail.com>
2022-07-15 00:33:14 +0200

    deploy flask to heroku

--- python-api/venvs/flask/app.py
+++ python-api/venvs/flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request, jsonify
+from flask_cors import CORS
 from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, ObjectType, gql, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
 from ariadne.asgi import GraphQL
@@ -34,6 +35,7 @@ def resolve_suggestion(*_, userInput):
     return { 'userInput': userInput }
 
 app = Flask(__name__)
+CORS( app )
 
 schema = make_executable_schema(type_defs, query, mutation)
 
@@ -60,3 +62,6 @@ def graphql_server():
 @app.route('/')
 def hello_world():
     return 'Hello!'
+
+if __name__ == '__main__':
+    app.run( debug=True, port=5000 )
